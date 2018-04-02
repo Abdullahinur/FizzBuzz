a6b958126e632562037d5c479bb04f056c6d653b
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-02 09:47:26 +0300

    create function to print range and iterate add print and welcome message add sign to last commit

---
+++ FizzBuzz.py
@@ -0,0 +1,15 @@
+print('Welcome to FizzBuzz, A simple python shell program that prints numbers from 1 to 100, But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.')
+print('Enter the last number in the range')
+maxNumber=int(input())
+print('Enter the starting number')
+i=int(input())
+
+for i in range(i,maxNumber+1):
+  if ((i%3==0) and (i%5==0)):
+    print ('fizz buzz')
+  elif i%3==0:
+    print ('fizz')
+  elif i%5==0:
+    print ('buzz')
+  else:
+    print (i)
