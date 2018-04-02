9fd46f51b0c070e13e0fa9d832ce116304f33f02
Abdullahinur <aabdullahinur@hotmail.com>
2018-04-02 09:51:41 +0300

    remove single print

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,7 +3,7 @@ print('Enter the last number in the range')
 maxNumber=int(input())
 print('Enter the starting number')
 i=int(input())
-
+print('hello')
 for i in range(i,maxNumber+1):
   if ((i%3==0) and (i%5==0)):
     print ('fizz buzz')
